2f017e1ee28535fea7a18ad844bb0511f6427be7
Tewobsta13 <tewobstal@gmail.com>
2025-12-13 07:58:10 +0000

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
---
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Header.css";
+import NetflixLogo from "../../assets/images/Netflix.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        {/* Left side */}
+        <div className="header_left">
+          <img src={NetflixLogo} alt="Netflix Logo" className="logo" />
+          <ul className="menu_items">
+            <li> </li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+
+        {/* Right side */}
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
